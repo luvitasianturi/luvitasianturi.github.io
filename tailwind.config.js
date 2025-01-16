24ef86a4b5075187272c4e568528ec5419d38375
luvitasianturi <luvitasianturi02@gmail.com>
2025-01-16 20:46:16 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */ module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: { center: true, padding: '16px' },
+    extend: {
+      colors: { primary: '#ff7cc4', secondary: '#64748b', dark: '#0f172a' },
+      screens: { '2xl': '1320px' },
+    },
+  },
+  plugins: [],
+};
